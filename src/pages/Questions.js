98b7bab5d7cf8943795ef8bd5e68cbb477c1f30c
rasanmemon <rasanmemon@gmail.com>
2022-07-14 00:20:32 +0500

    getdata

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,23 +1,25 @@
-import React, { useState,useEffect } from "react";
+import React, { useState,useEffect }  from "react";
 import axios from "axios";
 
 const Questions = ()=> {
     // console.log('Im Superman');
-    const [database,setdatabase] = useState({});
+    let [sdatabase,setsdatabase] = useState({});
+    const [loading, setloading] = useState(false);
     const mystyle={
         margin: "0 0 0 200px"
     }
     useEffect( () => {
         const conn = async() => {
+          
         try {
             await axios.get("http://localhost:8080/getQuestions")
           .then((responce) => {
             if (responce.data.length > 0) {
-             setdatabase([...responce.data])
-             console.log(responce.data);
-             console.log(database)
-            //  console.log(qdata)
-            //   setloading(true);
+             sdatabase=responce.data;
+             console.log(...responce.data);
+             //  console.log(qdata)
+             setloading(true);
+             console.log(sdatabase);
           }
              else {
               console.log("Question Fetch Failed")
@@ -28,6 +30,8 @@ const Questions = ()=> {
         }}
         conn()
       },[]);
+
+     
     return (
     <div class="w3-container w3-display-container" style={mystyle}  >
      {/* {Object.entries(database).map(([key, value]) => {
