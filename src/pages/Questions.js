bfbd8363a50020b771e7b945e17f6e9752ce8cd6
rasanmemon <rasanmemon@gmail.com>
2022-07-14 12:40:03 +0500

    datafatch

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -3,45 +3,39 @@ import axios from "axios";
 
 const Questions = ()=> {
     // console.log('Im Superman');
-    let [sdatabase,setsdatabase] = useState({});
+    const [data,setData] = useState([]);
     const [loading, setloading] = useState(false);
     const mystyle={
         margin: "0 0 0 200px"
     }
+    const getData = () => {
+      axios
+      .get('http://localhost:8080/getQuestions')
+      .then((res) => {
+        console.log(res);
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    }
     useEffect( () => {
-        const conn = async() => {
-          
-        try {
-            await axios.get("http://localhost:8080/getQuestions")
-          .then((responce) => {
-            if (responce.data.length > 0) {
-             sdatabase=responce.data;
-             console.log(...responce.data);
-             //  console.log(qdata)
-             setloading(true);
-             console.log(sdatabase);
-          }
-             else {
-              console.log("Question Fetch Failed")
-             }
-            })
-        } catch (error) {
-          console.error(error)
-        }}
-        conn()
-      },[]);
+      if(data && data.length === 0){
+        getData();
+      }
+    },[]);
 
      
     return (
-    <div class="w3-container w3-display-container" style={mystyle}  >
-     {/* {Object.entries(database).map(([key, value]) => {
-        return (
-            <div>
-                {key} = {value}
-            </div>
-        );
-    })} */}
-    
+    <div class="w3-container w3-display-container" style={mystyle} >
+     <ul>
+     {
+      data.map((dt,index)=>{
+        // return <li>{}</li>
+        return <li>{dt.Qno + ": "+ dt.Questions}</li>
+      })
+     }
+     </ul>         
     </div>
        
     );
