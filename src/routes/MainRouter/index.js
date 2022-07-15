b17db887e6df608d585c74a166393b37a3df8b59
rasanmemon <rasanmemon@gmail.com>
2022-07-15 16:44:26 +0500

    Update index.js

--- src/routes/MainRouter/index.js
+++ src/routes/MainRouter/index.js
@@ -1,9 +1,7 @@
 import React from "react";
-
 import { BrowserRouter as Router, Route ,Routes } from "react-router-dom";
 import routes from './../../config/routes'
 import ProtectedRoutes from './../ProtectedRoute'
-
 function MainRouter () {
   return (
     <Router>
